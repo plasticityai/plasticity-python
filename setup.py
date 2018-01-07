d956a7b7abcd44a0c038ab9794a2adbdf2c18e57
acsands13 <acsands13@gmail.com>
2018-01-06 16:21:04 -0800

    Update the setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='plasticity',
-    version='0.1',
-    description='A Python package for the Plasticity API.',
-    url='http://plasticity.ai/api',
-    author='Plasticity',
-    author_email='support@plasticity.ai',
-    license='MIT',
-    packages=['plasticity'],
-    zip_safe=False
+    name = 'plasticity',
+    version = '0.1',
+    description = 'A Python package for the Plasticity API.',
+    author = 'Plasticity',
+    author_email = 'support@plasticity.ai',
+    url = 'http://plasticity.ai/api',
+    keywords = ["plasticity","nlp"],
+    license = 'MIT',
+    extras_require = {
+        ':python_version<"3.0"': [
+            "requests[security] >= 2.0.0",
+        ],
+        ':python_version>="3.0"': [
+            "requests >= 2.0.0",
+        ],
+    },
+    packages = find_packages(exclude=['tests', 'tests.*']),
+    zip_safe = False
 )
