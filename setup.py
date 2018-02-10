2d3b07d28cc9364373e24682b8d94f957a18a639
acsands13 <acsands13@gmail.com>
2018-02-10 15:40:57 -0800

    Update setup.py and LICENSE

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup, find_packages
 
-
-# To install the twilio-python library, open a Terminal shell, then run this
-# file by typing:
-#
-# python setup.py install
-#
-# You need to have the setuptools module installed. Try reading the setuptools
-# documentation: http://pypi.python.org/pypi/setuptools
-
 setup(
     name='plasticity',
     packages=find_packages(exclude=['tests', 'tests.*']),
